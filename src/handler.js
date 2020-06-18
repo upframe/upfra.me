77a68857b8904fa7113b5873a0b7a08704b92ed2
Mathis Bullinger <mathis@bullinger.dev>
2020-06-18 11:04:17 +0200

    feat: load redirects from dynamodb

--- src/handler.js
+++ src/handler.js
@@ -1,14 +1,28 @@
+const { get } = require('./dynamodb')
+
 const offline = process.env.STAGE === 'offline'
+
+const routeCache = {}
 
 module.exports.url = async (event) => {
   const path = event.path.replace(/^\//, '')
+
+  let { to = path, expires = 7 * 24 * 60 ** 2 } =
+    (path &&
+      (routeCache[path] ||
+        (routeCache[path] = await get(path.replace(/\/$/, ''))))) ||
+    {}
+
+  if (!/^https?\:\/\//.test(to))
+    to = `${process.env.REDIRECT_TO}/${to.replace(/^\//, '')}`
+
+  console.log(`redirect ${path} to ${to}`)
+
   return {
     statusCode: offline ? 302 : 301,
     headers: {
-      ...(offline && {
-        'Cache-Control': `max-age=${7 * 24 * 60 ** 2}`,
-      }),
-      Location: `${process.env.REDIRECT_TO.replace(/\/$/, '')}/${path}`,
+      'Cache-Control': `max-age=${expires}`,
+      Location: to,
     },
   }
 }
